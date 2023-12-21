1f8c015f0a0b387cffeb0f1e1b362b361fe18b58
Isaac Rivera <developer@isaacrivera.com>
2023-12-21 14:14:43 +0100

    add css colors types, refactor sidebar adding history and connecting to api + redux store

--- src/components/LeftSideBar/LeftSideBar.tsx
+++ src/components/LeftSideBar/LeftSideBar.tsx
@@ -1,10 +1,17 @@
 'use client';
-import { FormEvent, useEffect, useState } from 'react';
+import { FC, FormEvent, useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { useLazyGetPokemonByNameQuery } from '@/api/pokemonService';
+import { addEntry, setStatus } from '@/redux/searchHistorySlice';
 
-const LeftSideBar = () => {
+type Dispatcher = ReturnType<typeof useAppDispatch>;
+
+interface SubcomponentProps {
+  dispatch: Dispatcher;
+}
+
+const SearchForm: FC<SubcomponentProps> = ({ dispatch }) => {
   const [searchName, setSearchName] = useState<string>('');
-
   const [triggerGetPokemon, { isLoading, isFetching, data: pokemon, error }] =
     useLazyGetPokemonByNameQuery();
 
@@ -12,75 +19,96 @@ const LeftSideBar = () => {
 
   useEffect(() => {
     if (loading) {
-      console.info('-> LeftSideBar.triggerGetPokemon(), LOADING...');
+      dispatch(setStatus('loading'));
     } else if (error) {
-      console.info('-> LeftSideBar.triggerGetPokemon(), ERROR:', error);
+      dispatch(setStatus('error'));
     } else if (pokemon) {
-      console.info('-> LeftSideBar.triggerGetPokemon(), POKEMON:', pokemon);
+      dispatch(addEntry(pokemon));
     }
-  }, [loading, pokemon, error]);
+  }, [loading, pokemon, error, dispatch]);
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.group('(***) LeftSideBar.handleSubmit()');
-    console.info('\t-> searchName:', searchName);
-    console.groupEnd();
     if (searchName) {
       triggerGetPokemon(searchName, true);
     }
-
     setSearchName('');
   }
+  return (
+    <search>
+      <form onSubmit={handleSubmit}>
+        <label
+          className="block"
+          htmlFor="search"
+        >
+          Find Pokemon
+        </label>
+        <div className="flex">
+          <input
+            className="border p-2 w-full"
+            id="search"
+            name="search"
+            type="text"
+            placeholder="Pokemon name..."
+            value={searchName}
+            onChange={(event) =>
+              setSearchName(event.target.value.toLowerCase())
+            }
+            autoComplete="off"
+          />
+          <button
+            type="submit"
+            className="search-button"
+            disabled={searchName.length === 0 || loading}
+          >
+            search
+          </button>
+        </div>
+      </form>
+    </search>
+  );
+};
+
+const SearchHistory: FC<SubcomponentProps> = ({ dispatch }) => {
+  const searchHistory = useAppSelector(
+    (state) => state.searchHistory.value.history,
+  );
+  return (
+    <>
+      {searchHistory.length > 0 && (
+        <nav className="mt-2">
+          <label>History</label>
+          <ul
+            className="list-inside list-[square]"
+            id="history"
+          >
+            {searchHistory.map((p, index) => (
+              <li key={p.id}>
+                <button
+                  className="history-entry-button"
+                  onClick={() => dispatch(addEntry(p))}
+                  disabled={index === 0}
+                >
+                  {p.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
+    </>
+  );
+};
+
+const LeftSideBar = () => {
+  const dispatch = useAppDispatch();
   return (
     <aside
       id="left-panel"
       className="left-sidebar"
     >
-      <search>
-        <form onSubmit={handleSubmit}>
-          <label
-            className="block"
-            htmlFor="search"
-          >
-            Find Pokemon
-          </label>
-          <div className="flex">
-            <input
-              className="border p-2 w-full"
-              id="search"
-              name="search"
-              type="text"
-              placeholder="Pokemon name..."
-              value={searchName}
-              onChange={(event) =>
-                setSearchName(event.target.value.toLowerCase())
-              }
-              autoComplete="off"
-            />
-            <button
-              type="submit"
-              className="bg-green hover:[&:not([disabled])]:text-white active:[&:not([disabled])]:brightness-110 p-2 border border-dark-gray border-l-transparent"
-              disabled={searchName.length === 0}
-            >
-              search
-            </button>
-          </div>
-        </form>
-      </search>
-      <nav className="mt-2">
-        <label>History</label>
-        <ul
-          className="list-inside list-[square]"
-          id="history"
-        >
-          <li>
-            <button onClick={console.log}>PIKACHU</button>
-          </li>
-          <li>
-            <button onClick={console.log}>SCYTHER</button>
-          </li>
-        </ul>
-      </nav>
+      <SearchForm dispatch={dispatch} />
+      <SearchHistory dispatch={dispatch} />
     </aside>
   );
 };
---
+++ src/types/ui.ts
@@ -0,0 +1,16 @@
+export type ColorNames =
+  | 'transparent'
+  | 'current'
+  | 'white'
+  | 'black'
+  | 'red'
+  | 'light-red'
+  | 'light-gray'
+  | 'gray'
+  | 'dark-gray'
+  | 'green'
+  | 'light-blue'
+  | 'blue'
+  | 'dark-blue'
+  | 'yellow'
+  | 'dark-yellow';
